6cbd2630e050a8280dcaf75f8694363c6db24d2e
= <=>
2019-12-15 15:26:36 +0500

    FFF source code added

---
+++ FFF_main.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.5.12;
+// ----------------------------------------------------------------------------
+// Owned contract
+// ----------------------------------------------------------------------------
+contract Owned {
+    address public owner;
+    address public newOwner;
+
+    event OwnershipTransferred(address indexed _from, address indexed _to);
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function transferOwnership(address _newOwner) public onlyOwner {
+        newOwner = _newOwner;
+    }
+}
